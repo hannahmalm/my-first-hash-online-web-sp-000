add063f0db84e133b53674bd889d00dcfcfa76cd
Hannah Malm <hannahemalm616@gmail.com>
2020-12-08 23:38:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,13 @@
 def my_hash
  rent = {“Monthly Rent” => 1200}
 end 
-end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, 
 
 
 end
